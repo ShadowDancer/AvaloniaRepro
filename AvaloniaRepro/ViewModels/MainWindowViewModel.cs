8782cfc9aac63ff5bb47b811fdd46af6c6dd608e
Przemysław Onak <przemekonak@gmail.com>
2020-07-27 17:43:12 +0200

    Repro for incorrect styling for combobox in gridView in 0.10-preview2

---
+++ AvaloniaRepro/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaRepro.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<List<string>> Items { get; }= new List<List<string>>()
+        {
+            new List<string>()
+            {
+                "Item1",
+                "Item2",
+                "Item3",
+                "Item4",
+                "Item5"
+            },
+            new List<string>()
+            {
+                "Item1",
+                "Item2",
+                "Item3",
+                "Item4",
+                "Item5"
+            },
+        };
+
+
+        public string Greeting => @"
+Example command to show semantics of byte fields
+they are similar to string fields, just do not have encoding
+";
+    }
+}
