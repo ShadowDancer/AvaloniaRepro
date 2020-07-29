51ebc3d184bb4749bc924495167ede955ed43d58
Przemysław Onak <przemekonak@gmail.com>
2020-07-29 15:41:42 +0200

    Base app

--- AvaloniaRepro/ViewModels/MainWindowViewModel.cs
+++ AvaloniaRepro/ViewModels/MainWindowViewModel.cs
@@ -6,30 +6,6 @@ namespace AvaloniaRepro.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public List<List<string>> Items { get; }= new List<List<string>>()
-        {
-            new List<string>()
-            {
-                "Item1",
-                "Item2",
-                "Item3",
-                "Item4",
-                "Item5"
-            },
-            new List<string>()
-            {
-                "Item1",
-                "Item2",
-                "Item3",
-                "Item4",
-                "Item5"
-            },
-        };
-
-
-        public string Greeting => @"
-Example command to show semantics of byte fields
-they are similar to string fields, just do not have encoding
-";
+        public string Greeting => @"Welcome to Avalonia";
     }
 }
