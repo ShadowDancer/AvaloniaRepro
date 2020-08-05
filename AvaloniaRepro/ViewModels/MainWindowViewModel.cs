1b492050e0b5f23f76ffc9269e068145d68abbaf
Przemysław Onak <przemekonak@gmail.com>
2020-08-05 14:15:55 +0200

    List ignores SelectedItems contents

--- AvaloniaRepro/ViewModels/MainWindowViewModel.cs
+++ AvaloniaRepro/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace AvaloniaRepro.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => @"Welcome to Avalonia";
+        public List<string> Items { get; } = new List<string>
+        {
+            "Item1",
+            "Item2",
+            "Item3",
+            "Item4"
+        };
+
+        public MainWindowViewModel()
+        {
+            SelectedItems.Add(Items[1]);
+        }
+
+        public ObservableCollection<string> SelectedItems { get; } = new ObservableCollection<string>();
     }
 }
